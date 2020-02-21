3002349f253247049a20169a55ba51f5c64fb6fe
shamsadgrokr <shamsad.alam@datagrokr.com>
2020-02-21 18:49:17 +0530

    dependency file added

---
+++ source_code/src/dependency_injector.py
@@ -0,0 +1,44 @@
+"""
+Dependency injection module
+"""
+import mysql.connector
+from src.datamanager.db_helper import DBHelper
+from src.datamanager.data_store import DBDatastore
+
+from src.domain.managers.quiz_manager import QuizManager
+
+
+class DIQuiz():
+    """
+    Dependency Injection class
+    """
+    def __init__(self):
+        """
+        Connection with mysql is done
+        """
+
+        self.db_connection = mysql.connector.connect(host='localhost',\
+                                         database='quiz',\
+                                         user='root',\
+                                         password='shamsad@123')
+
+        self.db_helper = DBHelper(self.db_connection)
+        self.db_datastore = DBDatastore(self.db_helper)
+
+    def get_db_helper_instance(self):
+        """
+        get db_helper instance
+        """
+        return self.db_helper
+
+    def get_db_datastore_instance(self):
+        """
+        get db_datasore instance
+        """
+        return self.db_datastore
+
+    def get_quiz_manager(self):
+        """
+        Get quiz manager instance
+        """
+        return QuizManager('quiz', self.get_db_datastore_instance())
